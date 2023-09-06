acb247b5dbe21116607a20a80455fd974256ae20
Shank <shashanksharma21@gmail.com>
2023-09-06 19:20:51 +0000

    assignment1 | part1 | done

---
+++ part1_learningir/c_programs/test1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c, d;
+  a = getchar();
+  b = 0;
+  c = a != b;
+  d = b / c;
+  return 0;
+}
---
+++ part1_learningir/c_programs/test3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c, d;
+  a = getchar();
+  b = 0;
+  if (a > 100) {
+    d = a / b;
+  } else {
+    c = b / a;
+  }
+  return 0;
+}
