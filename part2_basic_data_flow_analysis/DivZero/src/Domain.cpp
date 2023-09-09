0cd1997d5b72d4fa3d91c2ddd0c2fc1f2cf6866e
Shank <shashanksharma21@gmail.com>
2023-09-09 21:35:33 +0000

    assignment1 | part2 | domain implemented

--- part2_basic_data_flow_analysis/DivZero/src/Domain.cpp
+++ part2_basic_data_flow_analysis/DivZero/src/Domain.cpp
@@ -1,7 +1,141 @@
 #include "Domain.h"
+#include <stdexcept>
 
 //===----------------------------------------------------------------------===//
 // Abstract Domain Implementation
 //===----------------------------------------------------------------------===//
 
 /* Add your code here */
+
+#include "Domain.h"
+#include <cassert>
+
+using namespace dataflow;
+
+Domain::Domain() : Domain(Value = Domain::MaybeZero) {}
+
+Domain::Domain(Element V) : Value{V} {}
+
+Domain *Domain::add(Domain *E1, Domain *E2) {
+  // should never be called on uninitialized values
+  assert(E1->Value != Domain::Uninit);
+  assert(E2->Value != Domain::Uninit);
+
+  if (E1->Value == Domain::Zero && E2->Value == Domain::Zero) {
+    return new Domain(Domain::Zero);
+  }
+
+  if (E1->Value == Domain::NonZero && E2->Value == Domain::NonZero) {
+    return new Domain(Domain::NonZero);
+  }
+
+  return new Domain(Domain::MaybeZero);
+}
+
+Domain *Domain::sub(Domain *E1, Domain *E2) {
+  // should never be called on uninitialized values
+  assert(E1->Value != Domain::Uninit);
+  assert(E2->Value != Domain::Uninit);
+
+  if (E1->Value == Domain::Zero && E2->Value == Domain::Zero) {
+    return new Domain(Domain::Zero);
+  }
+
+  if (E1->Value == Domain::NonZero && E2->Value == Domain::NonZero) {
+    return new Domain(Domain::NonZero);
+  }
+
+  return new Domain(Domain::MaybeZero);
+}
+
+Domain *Domain::mul(Domain *E1, Domain *E2) {
+  // should never be called on uninitialized values
+  assert(E1->Value != Domain::Uninit);
+  assert(E2->Value != Domain::Uninit);
+
+  if (E1->Value == Domain::Zero || E2->Value == Domain::Zero) {
+    return new Domain(Domain::Zero);
+  }
+
+  if (E1->Value == Domain::NonZero && E2->Value == Domain::NonZero) {
+    return new Domain(Domain::NonZero);
+  }
+
+  return new Domain(Domain::MaybeZero);
+}
+
+Domain *Domain::div(Domain *E1, Domain *E2) {
+  // should never be called on uninitialized values
+  assert(E1->Value != Domain::Uninit);
+  assert(E2->Value != Domain::Uninit);
+
+  // TMP: placeholder error if divisor is zero or maybezero
+  assert(E2->Value != Domain::Zero);
+  assert(E2->Value != Domain::MaybeZero);
+
+  if (E1->Value == Domain::Zero && E2->Value == Domain::NonZero) {
+    return new Domain(Domain::Zero);
+  }
+
+  if (E1->Value == Domain::NonZero && E2->Value == Domain::NonZero) {
+    return new Domain(Domain::NonZero);
+  }
+
+  if (E1->Value == Domain::MaybeZero && E2->Value == Domain::MaybeZero) {
+    return new Domain(Domain::MaybeZero);
+  }
+
+  throw std::invalid_argument("should be unreachable!");
+}
+
+Domain *Domain::join(Domain *E1, Domain *E2) {
+  if (E1->Value == Domain::Zero && E2->Value == Domain::Zero) {
+    return new Domain(Domain::Zero);
+  }
+
+  if (E1->Value == Domain::NonZero && E2->Value == Domain::NonZero) {
+    return new Domain(Domain::NonZero);
+  }
+
+  return new Domain(Domain::MaybeZero);
+}
+
+// E1 <= E2
+bool Domain::order(Domain E1, Domain E2) {
+  // should never be called on uninitialized values
+  assert(E1.Value != Domain::Uninit);
+  assert(E2.Value != Domain::Uninit);
+
+  // TODO: UNINIT < (Z, NZ) < MBZ ?
+  if (E1.Value == E2.Value) {
+    return true;
+  }
+
+  if (E2.Value == Domain::MaybeZero) {
+    return true;
+  }
+
+  return false;
+}
+
+void Domain::print(raw_ostream &O) {
+  switch (Value) {
+  case Uninit:
+    O << "Uninit";
+    break;
+  case NonZero:
+    O << "NonZero";
+    break;
+  case Zero:
+    O << "Zero";
+    break;
+  case MaybeZero:
+    O << "MaybeZero";
+    break;
+  }
+}
+
+raw_ostream &operator<<(raw_ostream &O, Domain v) {
+  v.print(O);
+  return O;
+}
---
+++ part2_basic_data_flow_analysis/DivZero/test/simple1.opt.ll
@@ -0,0 +1,22 @@
+; ModuleID = 'simple1.ll'
+source_filename = "simple1.c"
+target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-pc-linux-gnu"
+
+; Function Attrs: noinline nounwind uwtable
+define dso_local i32 @main() #0 {
+entry:
+  %cmp = icmp eq i32 0, 0
+  %conv = zext i1 %cmp to i32
+  %div = sdiv i32 0, %conv
+  %div1 = sdiv i32 %div, %div
+  ret i32 0
+}
+
+attributes #0 = { noinline nounwind uwtable "disable-tail-calls"="false" "frame-pointer"="all" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0}
+!llvm.ident = !{!1}
+
+!0 = !{i32 1, !"wchar_size", i32 4}
+!1 = !{!"Ubuntu clang version 12.0.1-19ubuntu3"}
